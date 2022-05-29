1547fe7b517891cd1c212b70c7b89967b171a542
MateusOFCZ <mateusdacruz357@hotmail.com>
2022-05-29 15:23:56 -0300

    ⚙️Code Organization

--- src/assets/IAResponses.js
+++ src/assets/IAResponses.js
@@ -41,14 +41,38 @@ async function GetCurrentTime() {
 
 async function IAData() {
     const Data = [
-        { trigger: [`OI`, `OLA`, `BOM DIA`, `BOA TARDE`, `BOA NOITE`], response: [`Oie!`, `Olá!`, `Oii!`] },
-        { trigger: [`COMO VAI`, `TUDO BEM`, `COMO VOCE ESTA`, `COMO VOCE TA`], response: [`Estou bem e você?`, `Estou bem, obrigado, e você?`] },
-        { trigger: [`QUE HORAS SAO`, `ME DIGA A HORA`], response: [`Agora são ${moment().tz("America/Sao_Paulo").format('HH:mm')}.`] },
-        { trigger: [`QUE DIA E HOJE`, `ESTAMOS EM QUE DIA`], response: [`Hoje é ${moment().tz("America/Sao_Paulo").format('DD/MM/yyyy')}.`] },
-        { trigger: [`COTACAO DO DOLAR`, `QUANTO ESTA O DOLAR`, `QUANTO TA O DOLAR`], response: [`O dólar está ${await MoneyData('USD')}`] },
-        { trigger: [`COTACAO DO EURO`, `QUANTO ESTA O EURO`, `QUANTO TA O EURO`], response: [`O euro está ${await MoneyData('EUR')}`] },
-        { trigger: [`PREVISAO DO TEMPO PARA HOJE`, `PREVISÃO DO TEMPO DE HOJE`, `PREVISAO DO TEMPO HOJE`], response: [`A previsão do tempo para hoje em ${await UserData.Weather.name} é de ${await UserData.Weather.main.temp} graus e ${await UserData.Weather.weather[0].description}, com mínima de ${await UserData.Weather.main.temp_min} graus e máxima de ${await UserData.Weather.main.temp_max} graus. Tenha ${await GetCurrentTime()}.`, `A previsão do tempo para hoje em ${await UserData.Weather.name} é de ${await UserData.Weather.main.temp} graus e ${await UserData.Weather.weather[0].description}, com mínima de ${await UserData.Weather.main.temp_min} graus e máxima de ${await UserData.Weather.main.temp_max} graus.`] },
-        { trigger: [`SENSACAO TERMICA PARA HOJE`, `SENSACAO TERMICA DE HOJE`, `SENSACAO TERMICA HOJE`], response: [`A temperatura em ${await UserData.Weather.name} é de ${await UserData.Weather.main.temp} graus com sensação térmica de ${await UserData.Weather.main.feels_like} graus. Tenha ${await GetCurrentTime()}.`, `A temperatura em ${await UserData.Weather.name} é de ${await UserData.Weather.main.temp} graus com sensação térmica de ${await UserData.Weather.main.feels_like} graus.`] },
+        {
+            trigger: [`OI`, `OLA`, `BOM DIA`, `BOA TARDE`, `BOA NOITE`],
+            response: [`Oie!`, `Olá!`, `Oii!`]
+        },
+        {
+            trigger: [`COMO VAI`, `TUDO BEM`, `COMO VOCE ESTA`, `COMO VOCE TA`],
+            response: [`Estou bem e você?`, `Estou bem, obrigado, e você?`]
+        },
+        {
+            trigger: [`QUE HORAS SAO`, `ME DIGA A HORA`],
+            response: [`Agora são ${moment().tz("America/Sao_Paulo").format('HH:mm')}.`]
+        },
+        {
+            trigger: [`QUE DIA E HOJE`, `ESTAMOS EM QUE DIA`],
+            response: [`Hoje é ${moment().tz("America/Sao_Paulo").format('DD/MM/yyyy')}.`]
+        },
+        {
+            trigger: [`COTACAO DO DOLAR`, `QUANTO ESTA O DOLAR`, `QUANTO TA O DOLAR`],
+            response: [`O dólar está ${await MoneyData('USD')}`]
+        },
+        {
+            trigger: [`COTACAO DO EURO`, `QUANTO ESTA O EURO`, `QUANTO TA O EURO`],
+            response: [`O euro está ${await MoneyData('EUR')}`]
+        },
+        {
+            trigger: [`PREVISAO DO TEMPO PARA HOJE`, `PREVISÃO DO TEMPO DE HOJE`, `PREVISAO DO TEMPO HOJE`],
+            response: [`A previsão do tempo para hoje em ${await UserData.Weather.name} é de ${await UserData.Weather.main.temp} graus e ${await UserData.Weather.weather[0].description}, com mínima de ${await UserData.Weather.main.temp_min} graus e máxima de ${await UserData.Weather.main.temp_max} graus. Tenha ${await GetCurrentTime()}.`, `A previsão do tempo para hoje em ${await UserData.Weather.name} é de ${await UserData.Weather.main.temp} graus e ${await UserData.Weather.weather[0].description}, com mínima de ${await UserData.Weather.main.temp_min} graus e máxima de ${await UserData.Weather.main.temp_max} graus.`]
+        },
+        {
+            trigger: [`SENSACAO TERMICA PARA HOJE`, `SENSACAO TERMICA DE HOJE`, `SENSACAO TERMICA HOJE`],
+            response: [`A temperatura em ${await UserData.Weather.name} é de ${await UserData.Weather.main.temp} graus com sensação térmica de ${await UserData.Weather.main.feels_like} graus. Tenha ${await GetCurrentTime()}.`, `A temperatura em ${await UserData.Weather.name} é de ${await UserData.Weather.main.temp} graus com sensação térmica de ${await UserData.Weather.main.feels_like} graus.`]
+        },
     ]
 
     return Data;
